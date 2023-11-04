aad4e1480d4fc84578994cba85640b754723c341
harrry <chegeharrison10@gmail.com>
2023-11-04 19:49:19 +0300

    Intial commit

---
+++ saturdayShopSystem/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=20, blank=False,null=False)
+    qtty = models.CharField(max_length=15, blank=False, null=False)
+    price = models.CharField(max_length=10, blank=False, null=False)
+    desc = models.CharField(max_length=200, blank=False, null=False)
+
+def __str__(self):
+    return self.name
